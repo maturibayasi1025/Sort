3b993c6e0e8afbbb350b32d50d3b074ea4ec2ab2
miumiu0917 <miumiu917@gmail.com>
2016-10-14 11:46:25 +0900

    コンフィグファイルの定義とソートクラスの分離

--- src/jp/co/adglobe/sort/Main.java
+++ src/jp/co/adglobe/sort/Main.java
@@ -1,26 +1,38 @@
 package jp.co.adglobe.sort;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
-		// この下は変更しないでください
 		Sorter s = new Sorter();
+
+		Properties prop = new Properties();
+		String configfile = "config/.properties";
+		String[] filepaths = new String[3];
+		try {
+			prop.load(new FileInputStream(configfile));
+			filepaths[0] = prop.getProperty("filepath1");
+			filepaths[1] = prop.getProperty("filepath2");
+			filepaths[2] = prop.getProperty("filepath3");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return;
+		}
+
 		long startTime = System.currentTimeMillis();
-		s.doExec();
+
+		for(String filepath: filepaths) {
+			s.doExec(filepath);
+		}
+
 		long milliTime = System.currentTimeMillis() - startTime;
 
 		BigDecimal time = new BigDecimal(milliTime);
 		BigDecimal thousand = new BigDecimal(1000);
 
 		System.out.println("実行時間は【" + time.divide(thousand) + "秒】です。");
-		// ここより上は変更しないでください
-	}
-}
-
-
-class Sorter {
-	public void doExec() {
-		// このメソッド内に処理を記述
 	}
 }
---
+++ src/jp/co/adglobe/sort/Sorter.java
@@ -0,0 +1,19 @@
+package jp.co.adglobe.sort;
+
+class Sorter {
+
+	// データの出力先ディレクトリ
+	private static final String OUTPUT_DIR = "data/output/";
+
+	/*
+	 * ソート実行メソッド
+	 *
+	 * ソート済みのテキストファイルを出力します。
+	 *
+	 * @param filepath ソート対象のテキストファイルのパス
+	 */
+	public void doExec(String filepath) {
+		// このメソッド内に処理を記述
+		System.out.println(filepath);
+	}
+}
