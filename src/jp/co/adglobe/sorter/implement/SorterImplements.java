8c67d02a93358a9cab6b8beacfee3150a72372d1
maturibayasi1025 <maturibayasi1025@gmail.com>
2016-11-24 11:20:24 +0900

    0秒で作成、バブルとクイックを関数にラップしました。

--- src/jp/co/adglobe/sorter/implement/SorterImplements.java
+++ src/jp/co/adglobe/sorter/implement/SorterImplements.java
@@ -24,29 +24,27 @@ class SorterImplements implements Sorter {
 	 *
 	 * @param filepath ソート対象のテキストファイルのパス
 	 */
+
 	public void doExec(String filename)  {
 		// このメソッド内に処理を記述
 		try{
+
 		BufferedReader br = new BufferedReader(new FileReader(INPUT_DIR + filename));
 
 		int[] test = new int[100000];
 		String str;
 
-		for(int i = 0;i < test.length;i++)
+		for(int i = 0;i < test.length; ++i)
 		{
 			str = br.readLine();
 			test[i] = Integer.parseInt(str);
 		}
 
-		for(int s = 0; s < test.length - 1; s++){
-			for(int t = 0; t < test.length; t++){
-				if(test[t] > test[s]){
-					 int temp = test[t];
-					 test[t] = test[s];
-					 test[s] = temp;
-				}
-			}
-		}
+		// クイックソートの処理
+		quick_sort(test, 0, test.length - 1);
+
+		//バブルソートの処理
+		//bubble_sort(test);
 
 		br.close();
 
@@ -61,6 +59,7 @@ class SorterImplements implements Sorter {
 		pw.close();
 
 		}
+
 		catch(FileNotFoundException e)
 		{
 			System.out.println(e);
@@ -69,6 +68,63 @@ class SorterImplements implements Sorter {
 		{
 			System.out.println(e);
 		}
+	}
+
+	/*
+	 * バブルソート
+	 *
+	 * 横隣りにある二つの数字を比較し、大きければ位置を変える
+	 *
+	 */
+	static void bubble_sort(int []data)
+	{
+		for(int s = 0; s < data.length - 1; ++s)
+		{
+			for(int t = 0; t < data.length - 1; ++t)
+			{
+				if(data[t] > data[s])
+				{
+					int temp = data[t];
+					data[t] = data[s];
+					data[s] = temp;
+				}
+			}
+		}
+	}
+
+
+	/*
+	 *
+	 * クイックソート
+	 *
+	 * 最低値と最大値探す半分を基準に振り分け検索し、そのあとにすべて合わせる
+	 */
+	static void quick_sort(int []d, int left, int right){
+
+		if (left >= right) {
+            return;
+        }
+
+        int p = d[(left + right)/2];
+        int l = left;
+        int r = right;
+        int tmp;
+
+        while(l <= r) {
+            while(d[l] < p) { ++l; }
+            while(d[r] > p) { --r; }
+            if (l <= r) {
+                tmp = d[l];
+                d[l] = d[r];
+                d[r] = tmp;
+                ++l;
+                --r;
+            }
+        }
+
+        quick_sort(d, left, r);  // ピボットより左側をクイックソート
+        quick_sort(d, l, right); // ピボットより右側をクイックソート
+
 	}
 
 }
